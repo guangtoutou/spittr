7f8fe4ea3b2848ca36185b0c52ed758f2fd613c6
guangtoutou <ni.ningning@gmail.com>
2017-02-28 23:17:25 +0800

    add a SpittleController

---
+++ src/main/java/spittr/Spittle.java
@@ -0,0 +1,49 @@
+package spittr;
+
+import java.util.Date;
+
+public class Spittle {
+	private final Long id;
+	private final String message;
+	private final Date time;
+	private Double latitude;
+	private Double longtitude;
+
+	public Spittle(String message, Date time) {
+		this(message, time, null, null);
+	}
+
+	public Spittle(String message, Date time, Double longtitude, Double latitude) {
+		this.id = null;
+		this.message = message;
+		this.time = time;
+		this.longtitude = longtitude;
+		this.latitude = latitude;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public Double getLongtitude() {
+		return longtitude;
+	}
+	
+	@Override
+	public boolean equals(Object that){
+		return true;
+		//return EqualsBuilder.reflectionEquals(this, that,"id","time");
+	}
+}
